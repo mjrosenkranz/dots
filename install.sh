c3b00417c52639d7fb9e557c5c13a1ea5f6a9fe0
xen0ne <rosenkranz1999@gmail.com>
2019-11-26 15:44:53 -0500

    cleaned up after new void install

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-BASEDIR="$HOME/dots"
-ln -sf $BASEDIR/berry/ $HOME/.config/berry
-ln -sf $BASEDIR/nvim/ $HOME/.config/nvim
-ln -sf $BASEDIR/misc/inputrc $HOME/.inputrc
-ln -sf $BASEDIR/bash/bashrc $HOME/.bashrc
-ln -sf $BASEDIR/xfiles/xres $HOME/.Xresources
-ln -sf $BASEDIR/xfiles/xinit $HOME/.xinitrc
-source $BASEDIR/bash/bashrc
