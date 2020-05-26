d6b3b5a6dbe6de9fe1308ec8d1c0f581e78bd1d3
xenone <rosenkranz1999@gmail.com>
2020-05-26 17:06:55 -0400

    switched to fish

---
+++ fish/alias.fish
@@ -0,0 +1,3 @@
+alias vimrc "nvim $HOME/dots/nvim/init.vim"
+alias fishrc "nvim $HOME/dots/fish/"
+alias sourcef "source $HOME/dots/fish/config.fish"
--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 # path
 set -u fish_user_paths $HOME/dots/scripts/
+# colors
+clrmgr.py
 #
 set fish_greeting "Hi, xenone"
 
@@ -10,3 +12,23 @@ set fish_cursor_default block
 set fish_cursor_replace_one underscore
 set fish_cursor_visual block
 set -g prompt_emoji 💩
+
+
+set fish_color_normal white
+set fish_color_command red --bold
+set fish_color_quote brwhite
+set fish_color_redirection blue
+set fish_color_end brblack
+set fish_color_error yellow
+set fish_color_param white
+set fish_color_comment brblack
+set fish_color_match green
+set fish_color_selection brblack
+set fish_color_search_match brwhite
+set fish_color_operator blue
+set fish_color_escape green --bold
+set fish_color_autosuggestion brblack -i
+set fish_color_cancel yellow
+
+# source
+source $HOME/dots/fish/alias.fish
