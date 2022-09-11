b5f6028339ced484cbdcc82c041c6e10b0e6acf5
Morgan Rosenkranz <rosenkranz1999@gmai.com>
2022-09-11 14:08:13 -0400

    gui stuff

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,38 +1,41 @@
 return require('packer').startup(function()
-	-- packaging
-	use 'wbthomason/packer.nvim'
+  -- packaging
+  use 'wbthomason/packer.nvim'
 
-	-- lsp/completion
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/nvim-compe'
-	use 'nvim-treesitter/nvim-treesitter'
+  -- lsp/completion
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/nvim-compe'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-treesitter/nvim-treesitter-context'
 
-	-- languages
-	use 'ziglang/zig.vim'
+  -- languages
+  use 'ziglang/zig.vim'
 
-    -- git
-    use 'tpope/vim-fugitive'
-    use 'LnL7/vim-nix'
-	
-	-- telescope
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-    }
+  -- git
+  use 'tpope/vim-fugitive'
+  use 'LnL7/vim-nix'
 
-	-- tmux stuff
-	use 'christoomey/vim-tmux-navigator'
+  -- telescope
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
 
-    -- duh
-	use 'morhetz/gruvbox'
 
-    -- debuggin
-    use 'mfussenegger/nvim-dap'
-    use 'mfussenegger/nvim-dap-python'
-    use 'nvim-telescope/telescope-dap.nvim'
+  -- tmux stuff
+  use 'christoomey/vim-tmux-navigator'
 
-    use 'jpalardy/vim-slime'
+  -- duh
+  use 'morhetz/gruvbox'
 
-    use 'tikhomirov/vim-glsl'
+  -- debuggin
+  use 'mfussenegger/nvim-dap'
+  use 'mfussenegger/nvim-dap-python'
+  use 'nvim-telescope/telescope-dap.nvim'
+
+  use 'jpalardy/vim-slime'
+
+  use 'tikhomirov/vim-glsl'
   use 'smithbm2316/centerpad.nvim'
+  use 'beyondmarc/hlsl.vim'
 end)
