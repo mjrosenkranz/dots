2562127b2e9bf4401d3ef059fb59a867ead9ba2a
mjr <morgan@alloyenterprises.co>
2023-12-12 15:14:56 -0500

    changed theme back to macchiato

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,16 +1,16 @@
--- require("catppuccin").setup({
---     flavor = "macchiato",
---     integrations = {
---       gitgutter = true,
---     }
--- })
--- vim.cmd('colorscheme catppuccin-macchiato')
--- 
--- require('nvim-highlight-colors').setup {}
+require("catppuccin").setup({
+    flavor = "macchiato",
+    integrations = {
+      gitgutter = true,
+    }
+})
+vim.cmd('colorscheme catppuccin-macchiato')
 
-vim.g.ayucolor = "light"
-vim.cmd('colorscheme ayu')
-local group = 'Comment'
-local old_hl = vim.api.nvim_get_hl_by_name( group, true )
-local new_hl = vim.tbl_extend( 'force', old_hl, { italic=true } )
-vim.api.nvim_set_hl( 0, group, new_hl )
+require('nvim-highlight-colors').setup {}
+
+-- vim.g.ayucolor = "light"
+-- vim.cmd('colorscheme ayu')
+-- local group = 'Comment'
+-- local old_hl = vim.api.nvim_get_hl_by_name( group, true )
+-- local new_hl = vim.tbl_extend( 'force', old_hl, { italic=true } )
+-- vim.api.nvim_set_hl( 0, group, new_hl )
