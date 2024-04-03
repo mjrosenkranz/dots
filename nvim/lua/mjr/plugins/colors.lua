64a76719e0cab9186da5f687cbb601dac8c28287
mjr <morgan@alloyenterprises.co>
2024-04-03 11:19:01 -0400

    new color scheme kanagawa

--- nvim/lua/mjr/plugins/colors.lua
+++ nvim/lua/mjr/plugins/colors.lua
@@ -2,15 +2,28 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    init = function ()
-      require("catppuccin").setup({
-          flavor = "macchiato",
-          integrations = {
-            gitgutter = true,
-          }
-      })
-      vim.cmd('colorscheme catppuccin-macchiato')
-    end,
+    -- init = function ()
+    --   require("catppuccin").setup({
+    --       flavor = "macchiato",
+    --       integrations = {
+    --         gitgutter = true,
+    --       }
+    --   })
+    --   vim.cmd('colorscheme catppuccin-macchiato')
+    -- end,
   },
-  'brenoprata10/nvim-highlight-colors',
+  {
+    "rebelot/kanagawa.nvim",
+    init = function ()
+       vim.cmd('colorscheme kanagawa')
+    end
+  },
+  {
+    'brenoprata10/nvim-highlight-colors',
+    config = function () 
+      vim.api.nvim_create_user_command("HlColor",
+        function () require("nvim-highlight-colors").turnOn()
+      end, {})
+    end
+  } 
 }
