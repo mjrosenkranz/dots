539bd56a619624d17171f49b79ac8128f710732a
mjr <morgan@alloyenterprises.co>
2024-10-07 11:34:32 -0400

    added some more helpful mini modules

--- nvim/lua/mjr/plugins/mini.lua
+++ nvim/lua/mjr/plugins/mini.lua
@@ -3,7 +3,14 @@ return {
     'echasnovski/mini.nvim',
     version = '*',
     config = function()
-      -- don't show the mode we're in twice
+      -- a / i text objects
+      require('mini.ai').setup()
+      -- w and fam past the end of a line
+      require('mini.jump').setup()
+      require('mini.comment').setup()
+      require('mini.cursorword').setup({delay = 1000,})
+      require('mini.splitjoin').setup()
+      require('mini.surround').setup()
     end
   },
 }
