9f8d852bad065b8ed3944db22cab134b7f6566a0
mjr <morgan@alloyenterprises.co>
2025-03-05 14:04:46 -0500

    oil: show hidden files and don't prompt for simple edits

--- nvim/lua/mjr/plugins/nav.lua
+++ nvim/lua/mjr/plugins/nav.lua
@@ -1,7 +1,13 @@
 return {
   {
     'stevearc/oil.nvim',
-    opts = {},
+    opts = {
+      default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+      },
+    },
     -- Optional dependencies
     -- dependencies = { "nvim-tree/nvim-web-devicons" },
   },
