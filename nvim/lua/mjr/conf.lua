eab03db8b7886872cf5208a48a56584ccdc5e14a
mjr <morgan@alloyenterprises.co>
2025-03-06 10:36:07 -0500

    added spell checking to comments

--- nvim/lua/mjr/conf.lua
+++ nvim/lua/mjr/conf.lua
@@ -34,15 +34,5 @@ vim.api.nvim_command "packadd cfilter"
 
 vim.opt.shortmess = "filnxtToOFSW"
 
--- TODO: make a whole statusline setup
--- if vim.v.hlsearch == 1 then
--- 	local sinfo = vim.fn.searchcount { maxcount = 0 }
--- 	local search_stat = sinfo.incomplete > 0 and '[?/?]'
--- 		or sinfo.total > 0 and ('[%s/%s]'):format(sinfo.current, sinfo.total)
--- 		or nil
--- 
--- 	if search_stat ~= nil then
--- 		-- add search_stat to statusline/winbar
---     -- vim.wo.statusline = search_stat
--- 	end
--- end
+vim.opt.syntax = "enable"
+vim.opt.spell = true
